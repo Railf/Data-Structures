a3630bf7e5b56103f5cabcaec33cdf52e49e516d
Ralph McCracken, III <Ralph@McCracken.work>
2018-08-13 15:10:23 -0500

    Initial Commit

---
+++ Data-Structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data-Structures
+//
+//  Created by Railf on 8/13/18.
+//  Copyright © 2018 Railf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
