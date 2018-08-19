ada3ab249c32c70a551907d67c9da7107f77a40e
Ralph McCracken, III <Ralph@McCracken.work>
2018-08-18 22:08:26 -0500

    Initial tree creation.

---
+++ Data-Structures/tree.cpp
@@ -0,0 +1,9 @@
+//
+//  tree.cpp
+//  Data-Structures
+//
+//  Created by Railf on 8/18/18.
+//  Copyright © 2018 Railf. All rights reserved.
+//
+
+#include "tree.hpp"
---
+++ Data-Structures/tree.hpp
@@ -0,0 +1,14 @@
+//
+//  tree.hpp
+//  Data-Structures
+//
+//  Created by Railf on 8/18/18.
+//  Copyright © 2018 Railf. All rights reserved.
+//
+
+#ifndef tree_hpp
+#define tree_hpp
+
+#include <stdio.h>
+
+#endif /* tree_hpp */
