5e8f47694e3be4c45268ad5b494c861ac8e176e7
Ralph McCracken, III <Ralph@McCracken.work>
2018-08-19 23:54:50 -0500

    Tree<T>: Transitioning to header and implementation separation.

--- Data-Structures/tree.hpp
+++ Data-Structures/tree.hpp
@@ -14,7 +14,7 @@
 template <typename T>
 class Tree
 {
-  struct Node
+  typedef struct Node
   {
     T data_;
     Node* left_;
@@ -22,57 +22,21 @@ class Tree
     
     Node(): data_(0), left_(nullptr), right_(nullptr){}
     Node(T data): data_(data), left_(nullptr), right_(nullptr){}
-  };
+  } *treeNode;
   
-  Node* root_;
+  treeNode root_;
   
 public:
-  Tree(): root_(nullptr)
-  {}
+  Tree();
+  ~Tree();
   
-  ~Tree()
-  {
-    delete [] root_;
-  }
+  bool Empty();
   
-  bool Empty()
-  {
-    return (root_ == nullptr);
-  }
+  void Insert(treeNode newNode, treeNode curNode);
+  void Push(T data);
   
-  void Push(T data)
-  {
-    Node* newNode = new Node(data);
-    Node* curNode = root_;
-    
-    if (Empty())
-    {
-      root_ = newNode;
-    }
-    else if (newNode->data_ < curNode->data_)
-    {
-      curNode->left_ = newNode;
-    }
-    else if (newNode->data_ > curNode->data_)
-    {
-      curNode->right_ = newNode;
-    }
-    else
-    {
-      std::cerr << "** Tree<T>: " << data << " is already present in tree.\n";
-    }
-  }
-  
-  T    Root()
-  {
-    return root_->data_;
-  }
-  
-  void toString()
-  {
-    std::cout << root_->data_ << '\n';
-    std::cout << root_->left_->data_ << " " << root_->right_->data_ << '\n';
-  }
+  T Root();
+  void toString();
 };
 
 #include "tree.cpp"
